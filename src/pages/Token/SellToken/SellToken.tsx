65607e92284ea8f06de02e5b699b7e50a5be5c9c
drgosh <drgosh.m@gmail.com>
2022-09-23 08:32:31 +0300

    MAR-945 Fixed correct displaying Sell button for primary market

--- src/pages/Token/SellToken/SellToken.tsx
+++ src/pages/Token/SellToken/SellToken.tsx
@@ -1,11 +1,10 @@
-import React, { FC, useMemo } from 'react';
+import React, { FC } from 'react';
 import styled from 'styled-components';
 import { Button, Text } from '@unique-nft/ui-kit';
 
-import { AdditionalWarning100, AdditionalWarning500, Grey300 } from 'styles/colors';
+import { Grey300 } from 'styles/colors';
 import { Offer } from 'api/restApi/offers/types';
 import { Price } from '../TokenDetail/Price';
-import { useAccounts } from 'hooks/useAccounts';
 import { useApi } from 'hooks/useApi';
 import { useAdminLoggingIn } from '../../../api/restApi/admin/login';
 
@@ -19,13 +18,8 @@ interface SellTokenProps {
 }
 
 export const SellToken: FC<SellTokenProps> = ({ offer, isAllowed, onSellClick, onTransferClick, onDelistClick, testid }) => {
-  const { selectedAccount } = useAccounts();
   const { settings } = useApi();
   const { hasAdminPermission } = useAdminLoggingIn();
-
-  const hideSellBtn = useMemo(() => {
-    return settings?.marketType === 'primary' && !settings?.administrators.includes(selectedAccount?.address || '');
-  }, [settings, selectedAccount]);
 
   if (offer) {
     return (<>
@@ -48,7 +42,7 @@ export const SellToken: FC<SellTokenProps> = ({ offer, isAllowed, onSellClick, o
   return (
     <>
       <ActionsWrapper>
-        {(settings?.marketType !== 'primary' && hasAdminPermission) && <Button title={'Sell'} role={'primary'} onClick={onSellClick}/>}
+        {(settings?.marketType !== 'primary' || hasAdminPermission) && <Button title={'Sell'} role={'primary'} onClick={onSellClick}/>}
         <Button
           title={'Transfer'}
           onClick={onTransferClick}
